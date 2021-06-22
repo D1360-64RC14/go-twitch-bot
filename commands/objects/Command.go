a1647af1b5097b954a927bb0a261e54a97a832e9
D1360-64RC14 <gdiego416@gmail.com>
2021-06-21 22:47:26 -0300

    consertado problemas de sintaxe

--- commands/objects/Command.go
+++ commands/objects/Command.go
@@ -1,7 +1,6 @@
 package objects
 
 import (
-	"fmt"
 	"regexp"
 	"time"
 
@@ -65,8 +64,8 @@ func (c *Command) Exec(message twitch.PrivateMessage, client *twitch.Client, dat
 	if c.OnCooldownAll(message.User) {
 		if c.Cooldown.Behavior != nil {
 			if output := c.Cooldown.Behavior(message, client, database, c); len(output) > 0 {
-			chat.Say(client, message.Channel, output)
-		}
+				chat.Say(client, message.Channel, output)
+			}
 		}
 
 		return
