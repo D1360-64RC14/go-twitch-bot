f3ed65edcb5860944736b4734b86d9a792a93da1
D1360-64RC14 <gdiego416@gmail.com>
2021-06-23 08:56:15 -0300

    Adicionado documentação das funções

--- commands/objects/Command.go
+++ commands/objects/Command.go
@@ -43,6 +43,7 @@ type Command struct {
 	Behavior          CommandBehavior
 }
 
+// NewCommand cria uma instância do objeto 'Command'.
 func NewCommand(
 		name                string,
 		description         string,
@@ -71,6 +72,7 @@ func NewCommand(
 	}
 }
 
+// Validate verifica se o comando é encontrado na mensagem passada.
 func (c *Command) Validate(message twitch.PrivateMessage) bool {
 	if c.CaseSensitive {
 		return c.Pattern.MatchString(message.Message)
@@ -78,6 +80,7 @@ func (c *Command) Validate(message twitch.PrivateMessage) bool {
 	return c.Pattern.MatchString(strings.ToLower(message.Message))
 }
 
+// Exec executa o comportamento programado para o comando.
 func (c *Command) Exec(message twitch.PrivateMessage, client *twitch.Client, database *gorm.DB) {
 	if c.InCooldownAll(message.User) {
 		if c.Cooldown.Behavior != nil {
@@ -97,28 +100,36 @@ func (c *Command) Exec(message twitch.PrivateMessage, client *twitch.Client, dat
 	}
 }
 
+// InCooldownGlobal verifica se o comando
+// está em cooldown no escopo 'Global'.
 func (c *Command) InCooldownGlobal() bool {
 	var cooldown = c.Cooldown.globalSave
 
 	return checkCooldown(&cooldown)
 }
 
+// InCooldownUser verifica se o comando
+// está em cooldown no escopo de 'User'.
 func (c *Command) InCooldownUser(user twitch.User) bool {
 	var cooldown = c.Cooldown.userSave[user.ID]
 
 	return checkCooldown(&cooldown)
 }
 
+// InCooldownAll verifica se o comando
+// está em cooldown em ambos escopos 'Global' e 'User'.
 func (c *Command) InCooldownAll(user twitch.User) bool {
 	return c.InCooldownGlobal() || c.InCooldownUser(user)
 }
 
+// UpdateGlobalCooldown atualiza o cooldown do escopo 'Global'.
 func (c *Command) UpdateGlobalCooldown(secs uint32) {
 	var cooldown = time.Duration(secs)
 
 	c.Cooldown.globalSave = time.Now().Add(time.Second * cooldown)
 }
 
+// UpdateUserCooldown atualiza o cooldown do escolo de 'User'.
 func (c *Command) UpdateUserCooldown(user twitch.User, secs uint32) {
 	var cooldown = time.Duration(secs)
 
@@ -129,11 +140,14 @@ func (c *Command) UpdateUserCooldown(user twitch.User, secs uint32) {
 	c.Cooldown.userSave[user.ID] = time.Now().Add(time.Second * cooldown)
 }
 
+// UpdateCooldownAll atualiza o cooldown em ambos
+// escopos 'Global' e 'User', porém utiliza do tempo padrão.
 func (c *Command) UpdateCooldownAll(user twitch.User) {
 	c.UpdateGlobalCooldown(c.Cooldown.Global)
 	c.UpdateUserCooldown(user, c.Cooldown.User)
 }
 
+// checkCooldown verifica se o tempo dado já expirou ou não.
 func checkCooldown(cooldown *time.Time) bool {
 	if !(cooldown.Unix() < time.Now().Unix()) {
 		// Ainda em cooldown
@@ -152,7 +166,6 @@ func (c *Command) CheckPermission(user twitch.User) bool {
 	if len(c.PermissionLevel) == 0 {
 		return true
 	}
-
 
 	for _, p := range c.PermissionLevel {
 		initial = initial || user.Badges[p] == 1
