4e8263d5b2d50e825c78448ebce892b7daefed3f
D1360-64RC14 <gdiego416@gmail.com>
2021-06-23 08:39:56 -0300

    Comando de exemplo 'LastUse' removido

--- database/migrations.go
+++ database/migrations.go
@@ -1,25 +1,9 @@
 package database
 
 import (
-	"log"
-
-	"github.com/d1360-64rc14/twitch-bot/database/tables"
 	"gorm.io/gorm"
 )
 
 func Migrations(db *gorm.DB) {
-	// Cria a tabela 'LastUse' caso não exista.
-	if !db.Migrator().HasTable(&tables.LastUse{}) {
-		var err = db.Migrator().CreateTable(&tables.LastUse{})
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Adiciona um elemento à tabela caso não tenha.
-	// As colunas 'count' e 'last_time' já estão definidas
-	// como auto create '0' e 'time.Now()', respectivamente.
-	if db.Take(&tables.LastUse{}).Error == gorm.ErrRecordNotFound {
-		db.Create(&tables.LastUse{})
-	}
+	
 }
--- database/tables/database.tables.go
+++ database/tables/database.tables.go
@@ -1,8 +1,7 @@
 package tables
 
-import "time"
-
-type LastUse struct {
-	Count    uint64    `gorm:"unique;->;<-;default:0"`
-	LastTime time.Time `gorm:"unique;->;<-;autoCreateTime"`
-}
+// Example:
+// type LastUse struct {
+// 	Count    uint64    `gorm:"unique;->;<-;default:0"`
+// 	LastTime time.Time `gorm:"unique;->;<-;autoCreateTime"`
+// }
